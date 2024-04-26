7315fbd1aed0cf16cb4cd3133c0647ff6ff05162
gusalviarez <gusalv2001@gmail.com>
2024-04-26 18:35:28 -0400

    reduced size of capy image to fit in mobile

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -15,15 +15,15 @@ function Clicker() {
 
   return (
     <div className="flex flex-col md:space-y-4 h-screen mx-4 justify-center items-center">
-      <div className="w-96 flex space-x-6">
+      <div className="w-56 md:w-96 flex space-x-6">
         <span className="text-2xl md:text-6xl">Chiguires</span>
         <span className="text-2xl md:text-6xl">{Math.floor(count)}</span>
       </div>
       <p className="text-xl md:text-3xl">{Number.isInteger(cps) ? <span>{cps}</span> : <span>{cps.toFixed(1)}</span>}
         <span> cps</span>
       </p>
-      <img ref={capyRef} src={capy} className="w-96" onClick={handleClick} />
-      {number && <span ref={spanRef} style={styles} className="text-yellow-400 text-5xl" >+ {number} chiguire</span>}
+      <img ref={capyRef} src={capy} className="w-56 md:w-96" onClick={handleClick} />
+      {number && <span ref={spanRef} style={styles} className="text-yellow-400 text-xl md:text-5xl" >+ {number}</span>}
     </div>
   );
 }
