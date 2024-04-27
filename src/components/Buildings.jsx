35056ef4ab19a7842c9eefc9a43074c197cd9879
gusalviarez <gusalv2001@gmail.com>
2024-04-27 12:38:55 -0400

    add question svg

--- src/components/Buildings.jsx
+++ src/components/Buildings.jsx
@@ -2,8 +2,9 @@ import { useCounterStore } from '../store';
 import fish from "/fish-svgrepo-com.svg"
 import capy from "/capybara-svgrepo-com.svg"
 import dna from "/dna-svgrepo-com.svg"
+import question from "/question-mark-svgrepo-com.svg"
 
-const images = [null, capy, fish, dna, capy, capy, capy, capy, capy, capy, capy, capy, capy]
+const images = [null, capy, fish, dna, question, question, question, question, question, question, question, question, question]
 
 const convertNumber = (number) => {
 
@@ -22,7 +23,7 @@ function Buildings() {
 
   return (
     <div id="square" className="overflow-x-hidden overflow-y-scroll bg-white w-full md:w-80 border-2 rounded-lg max-h-96 flex flex-col justify-center items-center" >
-      <div id="square-children" className="relative pt-[29rem] md:pt-[50rem]">
+      <div id="square-children" className="relative pt-[29rem] md:pt-[46rem]">
       {Object.entries(buildings).map(([buildingId, buildingData]) => (
         <div key={buildingId} className="flex flex-row justify-center items-center w-80 mx-4 space-y-4 space-x-8">
           <img src={images[buildingId]} alt="un bebe chiguire" className="h-auto w-12 md:w-16" />
--- src/components/Mutation.jsx
+++ src/components/Mutation.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { useCounterStore } from '../store';
 
-const images = [null, null, null, null,null,null,null,null,null,null,null,null,null,null,null,null,null]
-
 const convertNumber = (number) => {
 
     if (number < 1000000) {
@@ -20,10 +18,10 @@ function Mutation() {
   const { mutations, buyMutation } = useCounterStore();
 ``
   return (
-    <div id="square-mutation" className="bg-white w-full md:w-80 border-2 rounded-lg h-96">
+    <div id="square-mutation" className="bg-white w-full md:w-80 border-2 flex justify-center rounded-lg h-96">
       <div id="square-mutation-children" className="md:ml-1 flex flex-col justify-center items-center">
         {Object.entries(mutations).map(([mutationId, mutationData]) => (
-          <div key={mutationId} className="w-2/3 m-1 h-auto w-full flex items-center justify-center border-2 rounded-lg">
+          <div key={mutationId} className="w-2/3 m-1 p-1 h-auto w-full flex items-center justify-center border-2 rounded-lg">
             <button onClick={() => {buyMutation(mutationId)}}>
               <p className="text-xs">{mutationData.name}</p>
               <p className="text-xs">costo {convertNumber(mutationData.cost)}</p>
